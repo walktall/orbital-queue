1f61f5aaad0d759dfe2369496b2c0fb178925d3c
WANG QIANG <rovingbreeze@gmail.com>
2018-06-02 16:26:08 +0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'config/new'
-
-  get 'config/create'
-
-  get 'config/show'
-
   get 'home/index'
   root 'home#index'
+
+  resources :config, only: [:show, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
